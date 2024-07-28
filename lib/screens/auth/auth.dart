3d7098f1cc61d3d3d294d3cb11fdfca75e0b2b3c
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-07-28 18:41:21 +0500

    Auth page

--- lib/screens/auth/auth.dart
+++ lib/screens/auth/auth.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:iconify_flutter/icons/uil.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+import 'package:iconify_flutter/icons/ic.dart';
+import 'package:memo_mind/common/theme/colors.dart';
+import 'package:memo_mind/common/theme/spacing.dart';
 import 'package:memo_mind/services/auth/auth_service.dart';
+
+
 
 class AuthenticationPage extends StatefulWidget {
   const AuthenticationPage({super.key});
@@ -14,9 +21,41 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ElevatedButton(
-          child: const Text('Continue with Google'),
-          onPressed: () async => AuthService().signInWithGoogle()
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Iconify(
+                Ic.round_edit_note, 
+                size: 120, 
+                color: AppColors.description,
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                child: const Padding(
+                  padding: EdgeInsets.all(AppSpacings.l),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Iconify(Uil.signin, size: 30, color: AppColors.description,),
+                      SizedBox(width: 15),
+                      Text('Sign in with Google',)
+                    ],
+                  ),
+                ),
+                onPressed: () async => AuthService().signInWithGoogle(),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                child: const Text('Continue as guest', 
+                  style: TextStyle(
+                    color: AppColors.description,
+                    fontSize: 16,)
+                ),
+                onPressed: () async => AuthService().signInAnon(),
+              )
+            ],
+          ),
         )
       ),
     );
