704f3bc04d923a5683a2c6e3a1af19469b8a25cc
Ycalk <118367882+Ycalk@users.noreply.github.com>
2024-08-13 18:06:15 +0500

    Change data updating method

--- lib/domain/provider.dart
+++ lib/domain/provider.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:memo_mind/domain/note.dart';
+import 'package:memo_mind/services/data/data_base.dart';
 
 
 class NoteProvider extends ChangeNotifier {
-  final List<Note> _notes = [];
-
+  List<Note> _notes = [];
+  late final DataBase database;
   Iterable<Note> get notes => _notes;
+
+  Future<void> setDatabase(DataBase database) async {
+    this.database = database;
+    database.collectionStream.listen((event) {
+      _notes = event;
+      notifyListeners();
+    });
+    _notes.clear();
+    _notes.addAll(await database.getNotes());
+    notifyListeners();
+  }
+  
+  NoteProvider();
 
   void add(Note note) {
     _notes.add(note);
